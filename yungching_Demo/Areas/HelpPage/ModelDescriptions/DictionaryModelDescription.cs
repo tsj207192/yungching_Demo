60f4e512590623f23c3613635f50431a3c695bce
袁國書 <tsj207192@gmail.com>
2021-01-07 21:50:16 +0900

    init project

---
+++ yungching_Demo/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace yungching_Demo.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ yungching_Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="yungching_Demo.WebApiApplication" Language="C#" %>
